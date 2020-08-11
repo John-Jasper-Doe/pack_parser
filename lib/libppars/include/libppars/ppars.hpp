afb5a3c9f368428db61e9ec7ad0e28aede9ea9a2
John Doe <john.jasper.doe@gmail.com>
2020-08-11 17:35:44 +0300

    Compile and include library

--- lib/libppars/include/libppars/ppars.hpp
+++ lib/libppars/include/libppars/ppars.hpp
@@ -1,10 +1,36 @@
+/**
+ * @file ppars.hpp
+ * @brief Discription of the "Parser" class.
+ *
+ * @author Maxim <john.jasper.doe@gmail.com>
+ * @date 2020
+ */
 
 #ifndef PPARS_HPP_
 #define PPARS_HPP_
 
+#include <memory>
+
+#include <libppars/core/ejector.hpp>
+#include <libppars/core/recipient.hpp>
+
+/** @brief The namespace of the "Packet Parse" library. */
 namespace ppars {
 
-class parser {};
+class parser {
+  using recipient_ptr_t = std::unique_ptr<core::recipient>;
+  using ejector_ptr_t = std::unique_ptr<core::ejector>;
+  using cntdat_ptr_t = std::shared_ptr<common::count_data>;
+
+  cntdat_ptr_t cntdat_;
+  recipient_ptr_t recipient_;
+  ejector_ptr_t ejector_;
+
+public:
+  explicit parser(unsigned short port) noexcept;
+
+  void main_loop() noexcept;
+};
 
 } /* ppars:: */
 
---
+++ lib/libppars/src/ppars.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file ppars.cpp
+ * @brief Implementation of the "Parser" class.
+ *
+ * @author Maxim <john.jasper.doe@gmail.com>
+ * @date 2020
+ */
+
+#include <libppars/ppars.hpp>
+
+#include <libppars/common/countdata.hpp>
+
+/** @brief The namespace of the "Packet Parse" library. */
+namespace ppars {
+
+parser::parser(unsigned short port) noexcept
+  : cntdat_{cntdat_ptr_t(new common::count_data())}
+  , recipient_{recipient_ptr_t(new core::recipient(port, cntdat_))}
+  , ejector_{ejector_ptr_t(new core::ejector(cntdat_))} {}
+
+void parser::main_loop() noexcept {
+  if (recipient_->open()) {
+    recipient_->exec();
+    ejector_->exec();
+
+    for (;;) {
+    }
+  }
+  else {
+    return;
+  }
+}
+
+} /* ppars:: */
